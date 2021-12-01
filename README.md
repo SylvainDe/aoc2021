faaf6087036d7a26bb76b3706fc71520a2d3a0fa
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-01 13:41:45 +0100

    Day 1 - part 1 & 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # aoc2021
 Advent Of Code 2021
 
-https://adventofcode.com/2021/day/1 : 0/2
+https://adventofcode.com/2021/day/1 : 2/2
--- day1.py
+++ day1.py
@@ -2,17 +2,29 @@
 import datetime
 
 
-def get_xxx_from_file(file_path="day1_input.txt"):
+def get_depths_from_file(file_path="day1_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        return [int(l.strip()) for l in f]
+
+
+def get_nb_increments(depths):
+    return sum(second > first for first, second in zip(depths, depths[1:]))
+
+
+def get_sliding_windows(depths):
+    return [sum(w) for w in zip(depths, depths[1:], depths[2:])]
 
 
 def run_tests():
-    pass
+    depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert get_nb_increments(depths) == 7
+    assert get_nb_increments(get_sliding_windows(depths)) == 5
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    depths = get_depths_from_file()
+    print(get_nb_increments(depths))
+    print(get_nb_increments(get_sliding_windows(depths)))
 
 
 if __name__ == "__main__":
