d0f21c3cde951c9e2794bbac71bd66322e27ff62
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-01 12:23:33 +0100

    Add scripts to start working on AOC problems easily
    Directly reused from https://github.com/SylvainDe/aoc2020 .

---
+++ all_days.py
@@ -0,0 +1,21 @@
+# vi: set shiftwidth=4 tabstop=4 expandtab:
+
+nb_days = 1
+days = [__import__("day%d" % i) for i in range(1, nb_days + 1)]
+
+
+def run_tests():
+    for day in days:
+        print("-", day.__name__)
+        day.run_tests()
+
+
+def get_solutions():
+    for day in days:
+        print("-", day.__name__)
+        day.get_solutions()
+
+
+if __name__ == "__main__":
+    run_tests()
+    get_solutions()
