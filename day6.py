95e09cbe7f1dca83df98d2c576c7c1c8c43817ce
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-06 09:21:40 +0100

    Day 6 - part 2

--- day6.py
+++ day6.py
@@ -1,6 +1,8 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
-
+import functools
+import itertools
+import collections
 
 def get_fishes_from_file(file_path="day6_input.txt"):
     with open(file_path) as f:
@@ -21,6 +23,20 @@ def n_generations(fishes, n):
     return fishes
 
 
+@functools.lru_cache(maxsize=100000)
+def nb_fishes_from_fish(fish, n):
+    if n == 0 or fish > n:
+        return 1
+    if fish == 0:
+        return nb_fishes_from_fish(6, n-1) + nb_fishes_from_fish(8, n-1)
+    return nb_fishes_from_fish(0, n - fish)
+
+
+def nb_fishes_from_fishes(fishes, n):
+     return sum(nb * nb_fishes_from_fish(val, n)
+                for val, nb in collections.Counter(fishes).items())
+
+
 def run_tests():
     fishes1 = [3, 4, 3, 1, 2]
     fishes2 = one_generation(fishes1)
@@ -33,11 +49,11 @@ def run_tests():
     assert n_generations(fishes1, 5) == [5,6,5,3,4,5,6,7,7,8]
     assert n_generations(fishes1, 6) == [4,5,4,2,3,4,5,6,6,7]
     assert n_generations(fishes1, 18) == [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]
-
+    print(nb_fishes_from_fishes(fishes1, 256) == 26984457539)
 
 def get_solutions():
     fishes = get_fishes_from_file()
-    print(len(n_generations(fishes, 80)))
+    print(nb_fishes_from_fishes(fishes, 256))
 
 
 if __name__ == "__main__":
