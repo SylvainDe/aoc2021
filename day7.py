8440c7e1d7a3a469f832f6defb0b32470ea0636d
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-07 12:14:20 +0100

    Day 7 - minor rewriting 3

--- day7.py
+++ day7.py
@@ -19,36 +19,40 @@ def dist2(pos1, pos2):
 def cost(crabs, pos, func):
     return sum(func(pos, crab) for crab in crabs)
 
-def get_best_position(crabs):
+def min_cost(crabs, func, positions_to_consider):
+    return min(cost(crabs, p, func) for p in positions_to_consider)
+
+def get_best_position_dist1(crabs):
     crabs = sorted(crabs)
     n = len(crabs)
     # Get cost around median position
-    return min(cost(crabs, crabs[cand], dist1) for cand in [n//2, (n+1)//2])
+    positions = (crabs[n//2], crabs[(n+1)//2])
+    return min_cost(crabs, dist1, positions)
 
-
-def get_best_position_dist_square(crabs):
-    # 1. Moving d costs (d+1)*d/2
-    #
-    # 2. Computing the minimal cost with a square cost: C = d²
+def get_best_position_dist2(crabs):
+    # 1. Computing the minimal cost with a square cost: C = d²
     #    can be done:
     #       F(x) = (p1 - x)² + (p2 - x)² + ... + (pn - x)²
     #       F(x) = nx² - 2x(p1 + p2 + ... + pn) + (p1² + p2² + ... + pn²)
     #       F²(x) = 2nx - 2(p1 + p2 + ... + pn)
     #       F'(x) = 0 <=> x = (p1 + ... + pn) / n
     #
-    # 3. Using the best solution for problem 2 works for problem 1 but I don't know why
+    # 2. Moving d cost C = (d+1)*d/2 which is slightly trickier to
+    #    optimise as it involves computing absolute values.
+    # 3. Using the best solution for problem 1 works for problem 2 but I don't know why
     avg = sum(crabs) / len(crabs)
-    return min(cost(crabs, cand, dist2) for cand in (math.floor(avg), math.ceil(avg)))
+    positions = (math.floor(avg), math.ceil(avg))
+    return min_cost(crabs, dist2, positions)
 
 def run_tests():
     crabs = [16,1,2,0,4,2,7,1,2,14]
-    assert get_best_position(crabs) == 37
-    assert get_best_position_dist_square(crabs) == 168
+    assert get_best_position_dist1(crabs) == 37
+    assert get_best_position_dist2(crabs) == 168
 
 def get_solutions():
     crabs = get_crabs_from_file()
-    print(get_best_position(crabs))
-    print(get_best_position_dist_square(crabs))
+    print(get_best_position_dist1(crabs))
+    print(get_best_position_dist2(crabs))
 
 
 if __name__ == "__main__":
