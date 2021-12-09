256df00580a5a00ef8d348355062dc5495f0b7c0
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-09 09:55:45 +0100

    Day 9 - part 1.5

--- day9.py
+++ day9.py
@@ -1,6 +1,6 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
-
+import collections
 
 def get_line_from_str(s):
     return [int(d) for d in s.strip()]
@@ -23,15 +23,50 @@ def neighbours(grid, x, y):
         if 0 <= x2 < len(grid):
             l = grid[x2]
             if 0 <= y2 < len(l):
-                yield l[y2]
+                yield x2, y2, l[y2]
 
+
+def get_low_points(grid):
+    return (
+        (x, y, val)
+        for x, y, val in points(grid)
+        if all(v > val for _, _, v in neighbours(grid, x, y))
+    )
+
+
+def print_point_sets(grid, points):
+    for x, line in enumerate(grid):
+        print("".join("X" if (x, y) in points else " " for y, val in enumerate(line)))
+ 
 
 def part1(grid):
-    return sum(
-        val + 1
-        for x, y, val in points(grid)
-        if all(v > val for v in neighbours(grid, x, y))
-    )
+    return sum(val + 1 for x, y, val in get_low_points(grid))
+
+
+def part2(grid):
+    flows = dict()
+    for x, y, val in points(grid):
+        lower_points = [(x2, y2) for x2, y2, v in neighbours(grid, x, y) if v < val]
+        # This is a miserunderstanding of the statement "flow to a single point": we should consider the final points
+        if len(lower_points) == 1:
+            x2, y2 = lower_points[0]
+            flows.setdefault((x2, y2), []).append((x, y))
+    print(flows)
+
+    basins = []
+    for x, y, _ in get_low_points(grid):
+        queue = collections.deque([(x, y)])
+        seen = set()
+        while queue:
+            p = queue.popleft()
+            if p not in seen:
+                seen.add(p)
+                for p2 in flows.get(p, []):
+                    queue.append(p2)
+        basins.append(len(seen))
+        print("basin found")
+        print_point_sets(grid, seen)
+    print(sorted(basins))
 
 
 def run_tests():
@@ -44,6 +79,7 @@ def run_tests():
     ]
     grid = [get_line_from_str(l) for l in grid]
     assert part1(grid) == 15
+    print(part2(grid))
 
 
 def get_solutions():
