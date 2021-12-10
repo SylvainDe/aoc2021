fe6a52c41a6c0f38dba5985ca0b682afa035b50b
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-10 08:41:51 +0100

    Day 10 - part 2

--- day10.py
+++ day10.py
@@ -2,6 +2,7 @@
 import datetime
 import collections
 
+
 def get_lines_from_file(file_path="day10_input.txt"):
     with open(file_path) as f:
         return [l.strip() for l in f]
@@ -14,14 +15,15 @@ for (a, b) in pairs:
     characters[a] = (b, 1)
     characters[b] = (b, -1)
 
-score = {
+corruption_score = {
     ")": 3,
     "]": 57,
     "}": 1197,
     ">": 25137,
 }
 
-def get_corrupted_score(s):
+
+def get_corruption_score(s):
     stack = list()
     for char in s:
         c, nb = characters[char]
@@ -30,32 +32,69 @@ def get_corrupted_score(s):
         else:
             last = stack.pop()
             if last != c:
-                return score[c]
+                return corruption_score[c]
     return 0
 
-def get_score(lines):
-    return sum(get_corrupted_score(l) for l in lines)
+
+completion_score = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+
+def get_completion_score(s):
+    stack = list()
+    for char in s:
+        c, nb = characters[char]
+        if nb == 1:
+            stack.append(c)
+        else:
+            last = stack.pop()
+            if last != c:
+                return 0
+    score = 0
+    for c in reversed(stack):
+        score *= 5
+        score += completion_score[c]
+    return score
+
+
+def get_corruption_final_score(lines):
+    return sum(get_corruption_score(l) for l in lines)
+
+
+def get_completion_final_score(lines):
+    scores = [get_completion_score(l) for l in lines]
+    scores = sorted([s for s in scores if s])
+    nb = len(scores)
+    return scores[nb // 2]
 
 
 def run_tests():
     lines = [
-    "[({(<(())[]>[[{[]{<()<>>",
-    "[(()[<>])]({[<{<<[]>>(",
-    "{([(<{}[<>[]}>{[]{[(<()>",
-    "(((({<>}<{<{<>}{[]{[]{}",
-    "[[<[([]))<([[{}[[()]]]",
-    "[{[{({}]{}}([{[{{{}}([]",
-    "{<[[]]>}<{[{[{[]{()[[[]",
-    "[<(<(<(<{}))><([]([]()",
-    "<{([([[(<>()){}]>(<<{{",
-    "<{([{{}}[<[[[<>{}]]]>[]]",
+        "[({(<(())[]>[[{[]{<()<>>",
+        "[(()[<>])]({[<{<<[]>>(",
+        "{([(<{}[<>[]}>{[]{[(<()>",
+        "(((({<>}<{<{<>}{[]{[]{}",
+        "[[<[([]))<([[{}[[()]]]",
+        "[{[{({}]{}}([{[{{{}}([]",
+        "{<[[]]>}<{[{[{[]{()[[[]",
+        "[<(<(<(<{}))><([]([]()",
+        "<{([([[(<>()){}]>(<<{{",
+        "<{([{{}}[<[[[<>{}]]]>[]]",
     ]
-    assert get_score(lines) == 26397
+    assert get_corruption_final_score(lines) == 26397
+    assert get_completion_score("[({(<(())[]>[[{[]{<()<>>") == 288957
+    assert get_completion_score("<{([{{}}[<[[[<>{}]]]>[]]") == 294
+    assert get_completion_final_score(lines) == 288957
 
 
 def get_solutions():
     lines = get_lines_from_file()
-    print(get_score(lines))
+    print(get_corruption_final_score(lines))
+    print(get_completion_final_score(lines))
 
 
 if __name__ == "__main__":
