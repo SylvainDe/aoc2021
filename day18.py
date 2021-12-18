6091bd3604ba666fbcbc24605ecb15cd1503669e
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-18 20:41:38 +0100

    Day 18 - Part 1
    Forget about previous data structure - start again

--- day18.py
+++ day18.py
@@ -1,24 +1,10 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import ast
-
-
-def is_pair(pair):
-    return isinstance(pair, list)
-
-
-def get_python_pair_from_str(s):
-    for c in s:
-        assert c in "[]1234567890,"
-    return ast.literal_eval(s)
+import collections
 
 
 def get_pair_from_str(s):
-    # /!\ This representation is slightly ambiguous and cannot make a difference between:
-    # [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
-    # [[3,[2,[1,[7,3]]],6,[5,[4,[3,2]]]]]
-    # Maybe using the fact that we are representing *pairs* and not random list could help
-    depth = 0
     digits = []
     pair = []
     for c in s:
@@ -26,39 +12,15 @@ def get_pair_from_str(s):
             digits.append(c)
         else:
             if digits:
-                pair.append([int("".join(digits)), depth])
-            digits = []
-            if c == "[":
-                depth += 1
-            elif c == "]":
-                depth -= 1
-            else:
-                assert c == ","
-    assert depth == 0
+                pair.append(int("".join(digits)))
+                digits = []
+            pair.append(c)
     assert digits == []
     return pair
 
 
 def get_str_from_pair(pair):
-    ret = []
-    last_depth = 0
-    first = True
-    for n, depth in pair:
-        if depth > last_depth:
-            if not first:
-                ret.append(",")
-            ret.append("[" * (depth - last_depth))
-        elif depth < last_depth:
-            ret.append("]" * (last_depth - depth))
-            if not first:
-                ret.append(",")
-        else:
-            ret.append(",")
-        ret.append(str(n))
-        last_depth = depth
-        first = False
-    ret.append("]" * last_depth)
-    return "".join(ret)
+    return "".join(str(e) for e in pair)
 
 
 def get_pairs_from_file(file_path="day18_input.txt"):
@@ -67,30 +29,50 @@ def get_pairs_from_file(file_path="day18_input.txt"):
 
 
 def explode(pair):
-    for i, (n, d) in enumerate(pair):
-        if d >= 5:
-            assert d == 5
-            assert 0 <= i + 1 < len(pair)
-            (n2, d2) = pair[i + 1]
-            assert d2 == 5
-            if 0 <= i - 1 < len(pair):
-                pair[i - 1][0] += n
-            if 0 <= i + 2 < len(pair):
-                pair[i + 2][0] += n2
-            pair[i + 1] = [0, d2 - 1]
-            pair.pop(i)
-            return True
+    nb_pos = []
+    explo = None
+    depth = 0
+    for i, e in enumerate(pair):
+        if e == "[":
+            depth += 1
+        elif e == "]":
+            depth -= 1
+        elif e == ",":
+            pass
+        else:
+            assert isinstance(e, int)
+            nb_pos.append(i)
+            if depth >= 5:
+                assert depth == 5
+                if explo is None:
+                    explo = len(nb_pos) - 1
+    assert depth == 0
+    if explo is not None:
+        i = nb_pos[explo]
+        assert 0 <= explo < len(nb_pos)
+        assert 0 <= explo + 1 < len(nb_pos)
+        assert nb_pos[explo + 1] == i + 2
+        if 0 <= explo - 1 < len(nb_pos):
+            pair[nb_pos[explo - 1]] += pair[i]
+        if 0 <= explo + 2 < len(nb_pos):
+            pair[nb_pos[explo + 2]] += pair[nb_pos[explo + 1]]
+        pair.pop(i - 1)
+        pair.pop(i - 1)
+        pair.pop(i - 1)
+        pair.pop(i - 1)
+        pair[i - 1] = 0
+        return True
     return False
 
 
 def split(pair):
-    for i, (n, d) in enumerate(pair):
-        if n >= 10:
-            half, rem = divmod(n, 2)
+    for i, e in enumerate(pair):
+        if isinstance(e, int) and e >= 10:
+            half, rem = divmod(e, 2)
             left, right = half, half + rem
             pair.pop(i)
-            pair.insert(i, [right, d + 1])
-            pair.insert(i, [left, d + 1])
+            for item in reversed(["[", left, ",", right, "]"]):
+                pair.insert(i, item)
             return True
     return False
 
@@ -105,10 +87,7 @@ def reduce_pair(pair):
 
 
 def add_pair(pair1, pair2):
-    pair = []
-    for p in [pair1, pair2]:
-        for n, d in p:
-            pair.append([n, d + 1])
+    pair = ["["] + pair1 + [","] + pair2 + ["]"]
     reduce_pair(pair)
     return pair
 
@@ -124,8 +103,28 @@ def add_pairs(lst):
 
 
 def magnitude(pair):
-    pass
-    # here we are stuck because of our representation of data
+    if len(pair) == 1:
+        assert isinstance(pair[0], int)
+        return pair[0]
+    depth = 0
+    coma = None
+    for i, e in enumerate(pair):
+        if e == "[":
+            depth += 1
+        elif e == "]":
+            depth -= 1
+        elif isinstance(e, int):
+            pass
+        else:
+            assert e == ","
+            if depth == 1:
+                assert coma is None
+                coma = i
+    assert depth == 0
+    assert coma is not None
+    left = pair[1:coma]
+    right = pair[coma + 1 : -1]
+    return 3 * magnitude(left) + 2 * magnitude(right)
 
 
 def string_handling_tests():
@@ -133,72 +132,69 @@ def string_handling_tests():
         "[[[[[9,8],1],2],3],4]",
         "[7,[6,[5,[4,[3,2]]]]]",
         "[[6,[5,[4,[3,2]]]],1]",
+        "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
+        "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+        "[[9,1],[1,9]]",
     ]
     for s in tests:
         pair = get_pair_from_str(s)
         s2 = get_str_from_pair(pair)
         assert s == s2
-    # These cases do not lead to same string
-    tests = [
-        "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
-        "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
-    ]
-    for s in tests:
-        pair = get_pair_from_str(s)
-        s2 = get_str_from_pair(pair)
-        pair2 = get_pair_from_str(s2)
-        assert pair == pair2
 
 
 def explode_tests():
     tests = [
-        "[[[[[9,8],1],2],3],4]",
-        "[7,[6,[5,[4,[3,2]]]]]",
-        "[[6,[5,[4,[3,2]]]],1]",
-        "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
-        "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+        ("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
+        ("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+        ("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"),
+        ("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+        ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
     ]
-    for s in tests:
-        pair = get_pair_from_str(s)
+    for s1, s2 in tests:
+        pair = get_pair_from_str(s1)
         assert explode(pair)
-        # print(get_str_from_pair(pair))
+        assert get_str_from_pair(pair) == s2
 
 
 def split_tests():
     tests = [
-        "[[[[0,7],4],[15,[0,13]]],[1,1]]",
+        ("[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"),
+        ("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"),
     ]
-    for s in tests:
+    for s, s2 in tests:
         pair = get_pair_from_str(s)
         assert split(pair)
-        print(get_str_from_pair(pair))
-        assert split(pair)
-        print(get_str_from_pair(pair))
+        assert get_str_from_pair(pair) == s2
 
 
 def reduce_tests():
     s = "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
+    s2 = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
     pair = get_pair_from_str(s)
     reduce_pair(pair)
-    print(get_str_from_pair(pair))
+    assert get_str_from_pair(pair) == s2
 
 
 def add_tests():
     p1 = get_pair_from_str("[[[[4,3],4],4],[7,[[8,4],9]]]")
     p2 = get_pair_from_str("[1,1]")
     pair = add_pair(p1, p2)
-    print(get_str_from_pair(pair))
+    s = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+    assert get_str_from_pair(pair) == s
 
 
 def adds_tests():
-    lst = ["[1,1]", "[2,2]", "[3,3]", "[4,4]"]
+    lst = ["[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]"]
     pairs = [get_pair_from_str(s) for s in lst]
     pair = add_pairs(pairs)
-    print(get_str_from_pair(pair))
+    assert get_str_from_pair(pair) == "[[[[3,0],[5,3]],[4,4]],[5,5]]"
 
 
 def magnitude_tests():
     tests = [
+        ("[9,1]", 29),
+        ("[1,9]", 21),
+        ("[[9,1],[1,9]]", 129),
         ("[[1,2],[[3,4],5]]", 143),
         ("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384),
         ("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445),
@@ -208,6 +204,7 @@ def magnitude_tests():
     ]
     for s, m in tests:
         pair = get_pair_from_str(s)
+        assert magnitude(pair) == m
 
 
 def run_tests():
@@ -222,6 +219,8 @@ def run_tests():
 
 def get_solutions():
     pairs = get_pairs_from_file()
+    pair = add_pairs(pairs)
+    print(magnitude(pair))
 
 
 if __name__ == "__main__":
