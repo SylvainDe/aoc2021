2b24f7d9a5e1b508b6932fb085da282f2dae8219
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-21 09:05:20 +0100

    Day 21 - part 1 - cleanup

--- day21.py
+++ day21.py
@@ -3,9 +3,13 @@ import datetime
 import itertools
 
 
+def get_position_from_line(s):
+    return int(s.split(": ")[-1])
+
+
 def get_positions_from_file(file_path="day21_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        return [get_position_from_line(l.strip()) for l in f]
 
 
 def deterministic_dice(sides=100):
@@ -19,29 +23,31 @@ def get_sum_rolls(dice, n):
 
 
 def game(positions):
-    # Shift positions by 1 to go from [1, 10] to [0, 9]
     nb_player = len(positions)
-    positions = [pos - 1 for pos in positions]
-    scores = [0 for pos in positions]
+    # Shift positions by 1 to go from [1, 10] to [0, 9]
+    players = [(pos - 1, 0) for pos in positions]
     dice = deterministic_dice()
     for i in itertools.count():
         player = i % nb_player
-        new_pos = (positions[player] + get_sum_rolls(dice, 3)) % 10
-        new_score = scores[player] + new_pos + 1
-        positions[player] = new_pos
-        scores[player] = new_score
-        if new_score >= 1000:
+        pos, score = players[player]
+        pos += get_sum_rolls(dice, 3)
+        pos %= 10
+        score += pos + 1
+        players[player] = (pos, score)
+        if score >= 1000:
             break
     nb_rolls = 3 * (i + 1)
-    return min(scores) * nb_rolls
+    return min([s for p, s in players]) * nb_rolls
 
 
 def run_tests():
-    assert game([4, 8]) == 739785
+    positions = [4, 8]
+    assert game(positions) == 739785
 
 
 def get_solutions():
-    print(game([9, 6]))
+    positions = get_positions_from_file()
+    print(game(positions))
 
 
 if __name__ == "__main__":
