3a6c5fe79fbe24e5f2b92ec09bfcfc626bb8e235
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-21 09:59:34 +0100

    Day 21 - draft for part 2

--- day21.py
+++ day21.py
@@ -1,6 +1,7 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import itertools
+import collections
 
 
 def get_position_from_line(s):
@@ -12,7 +13,7 @@ def get_positions_from_file(file_path="day21_input.txt"):
         return [get_position_from_line(l.strip()) for l in f]
 
 
-def deterministic_dice(sides=100):
+def deterministic_dice(sides):
     while True:
         for i in range(sides):
             yield i + 1
@@ -22,11 +23,11 @@ def get_sum_rolls(dice, n):
     return sum(next(dice) for _ in range(n))
 
 
-def game(positions, final_score=1000, rolls=3):
+def game(positions, final_score=1000, rolls=3, sides=100):
     nb_player = len(positions)
     # Shift positions by 1 to go from [1, 10] to [0, 9]
     players = [(pos - 1, 0) for pos in positions]
-    dice = deterministic_dice()
+    dice = deterministic_dice(100)
     for i in itertools.count():
         player = i % nb_player
         pos, score = players[player]
@@ -39,9 +40,58 @@ def game(positions, final_score=1000, rolls=3):
             return min([s for p, s in players]) * nb_rolls
 
 
+def get_sum_quantum_dice(sides, nb_rolls):
+    count = collections.Counter([0])
+    for _ in range(nb_rolls):
+        count2 = collections.Counter()
+        for val, nb in count.items():
+            for s in range(sides):
+                count2[val + s + 1] += nb
+        count = count2
+    return count
+
+
+def my_dict_pop(d):
+    for k, v in d.items():
+        d.pop(k)
+        return k, v
+    assert False
+
+
+def game2(positions, final_score=21, rolls=3, sides=3):
+    # TODO: Does not quite work
+    nb_player = len(positions)
+    nb_wins = [0 for _ in positions]
+    # Shift positions by 1 to go from [1, 10] to [0, 9]
+    players = [(pos - 1, 0) for pos in positions]
+    # Store games in a counter to handle multiple games at once
+    ongoing_games = collections.Counter([(tuple(players), 0)])
+    # Compute properties of dice just once
+    dice_count = get_sum_quantum_dice(sides, rolls)
+    while ongoing_games:
+        game, count = my_dict_pop(ongoing_games)
+        players, player_idx = game
+        player = players[player_idx]
+        next_player_idx = (player_idx + 1) % nb_player
+        for val, count2 in dice_count.items():
+            count3 = count * count2
+            pos, score = player
+            pos += val
+            pos % 10
+            score += pos + 1  # Shift position back to compute score
+            if score >= final_score:
+                nb_wins[player_idx] += count3
+            else:
+                players_lst = list(players)
+                players_lst[player_idx] = (pos, score)
+                ongoing_games[(tuple(players_lst), next_player_idx)] += count3
+    print(nb_wins)
+
+
 def run_tests():
     positions = [4, 8]
     assert game(positions) == 739785
+    print(game2(positions))
 
 
 def get_solutions():
